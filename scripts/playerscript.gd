9f05bb8b2bb08b953c5856e98c731d0a4c5b5147
matgib38 <82478253+matgib38@users.noreply.github.com>
2022-03-09 09:58:37 +1300

    completed movement code
    added movement code in 4 directions, with animations complete with idle.

--- scripts/playerscript.gd
+++ scripts/playerscript.gd
@@ -6,29 +6,47 @@ var direction: Vector2 = Vector2()
 #variable controling whether the camera follows the player
 var radius_y
 
-func read_input():
+func get_input():
 	velocity = Vector2()
+
 	
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 		direction = Vector2(0, -1)
+		$AnimationPlayer.play("walk_up")
 		
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
 		direction = Vector2(0, 1)
+		$AnimationPlayer.play("walk_down")
 		
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
 		direction = Vector2(-1, 0)
+		$AnimationPlayer.play("walk_left")
 		
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
 		direction = Vector2(1, 0)
+		$AnimationPlayer.play("walk_right")
+	
+	else:
+		if Input.is_action_just_released("up"):
+			$AnimationPlayer.play("idle_up")
 		
+		if Input.is_action_just_released("down"):
+			$AnimationPlayer.play("idle_down")
+		
+		if Input.is_action_just_released("left"):
+			$AnimationPlayer.play("idle_left")
+		
+		if Input.is_action_just_released("right"):
+			$AnimationPlayer.play("idle_right")
+	
 	velocity = velocity.normalized()
 	velocity = move_and_slide(velocity * 200)
 
 func _physics_process(delta):
-	read_input()
+	get_input()
 	
 	
