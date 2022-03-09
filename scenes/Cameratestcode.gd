3be8957bd2060b7398292079d2632769ef81ed17
Spikie147 <codbro75@kingshigh.school.nz>
2022-03-10 11:00:04 +1300

    Camera Testing

---
+++ scenes/Cameratestcode.gd
@@ -0,0 +1,8 @@
+extends Camera2D
+
+#controls the camera's position in relation to the player
+
+func _process(delta):
+	var player = get_parent().get_node("playercharactertest")
+	position.x = player.position.x
+	position.y = player.position.y
