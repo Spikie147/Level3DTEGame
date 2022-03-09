c357134c244a254e7e90b550127bf1af3692acc8
Spikie147 <codbro75@kingshigh.school.nz>
2022-03-10 10:11:31 +1300

    Errors fixed
    df

--- scenes/EnemyOrcCode.gd
+++ scenes/EnemyOrcCode.gd
@@ -1,11 +1,11 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 func _ready():
 	$AnimatedSprite.playing = true
 	var mob_types = $AnimatedSprite.frames.get_animation_names()
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
-	pass
+
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
-	pass
+
