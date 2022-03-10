2997318dcd321558e88da4c3d57547f08bb80b73
matgib38 <82478253+matgib38@users.noreply.github.com>
2022-03-11 12:12:38 +1300

    Player code edits
    Edited the players movement code, and moved some assets into folders for organisation

--- scripts/playerscript.gd
+++ scripts/playerscript.gd
@@ -3,43 +3,34 @@ extends KinematicBody2D
 var velocity: Vector2 = Vector2()
 var direction: Vector2 = Vector2()
 
-#variable controling whether the camera follows the player
-var radius_y
-
 func get_input():
 	velocity = Vector2()
-
-	
+	#determines how the character moves based on player inputs
+	#also plays specific animations depedning on the direction
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 		direction = Vector2(0, -1)
 		$AnimationPlayer.play("walk_up")
-		
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
 		direction = Vector2(0, 1)
 		$AnimationPlayer.play("walk_down")
-		
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
 		direction = Vector2(-1, 0)
 		$AnimationPlayer.play("walk_left")
-		
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
 		direction = Vector2(1, 0)
 		$AnimationPlayer.play("walk_right")
-	
+	#plays idle animation if the player is not moving
 	else:
 		if Input.is_action_just_released("up"):
 			$AnimationPlayer.play("idle_up")
-		
 		if Input.is_action_just_released("down"):
 			$AnimationPlayer.play("idle_down")
-		
 		if Input.is_action_just_released("left"):
 			$AnimationPlayer.play("idle_left")
-		
 		if Input.is_action_just_released("right"):
 			$AnimationPlayer.play("idle_right")
 	
@@ -48,5 +39,3 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-	
-	
