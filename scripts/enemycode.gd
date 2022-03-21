c984a06018a3a85155de302167aa3542fad3e647
Spikie147 <codbro75@kingshigh.school.nz>
2022-03-22 12:24:51 +1300

    added attack animation for enemy

--- scripts/enemycode.gd
+++ scripts/enemycode.gd
@@ -1,10 +1,25 @@
 extends KinematicBody2D
 
+const GRAVITY =10
+export(int) var SPEED =20
+const FLOOR=Vector2(0,1)
+var velocity=Vector2()
+var direction=1
 
-func _ready():
-	$AnimatedSprite.playing = true
-	var mob_types = $AnimatedSprite.frames.get_animation_names()
-	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+func _physics_process(delta):
+	velocity.x=SPEED*direction
+	if direction == 1:
+		$AnimatedSprite.play("walk_right")
+		$AnimatedSprite.flip_h=false 
+	else:
+		$AnimatedSprite.play("walk_left")
+		velocity.y+=GRAVITY
+		velocity=move_and_slide(velocity, FLOOR)
 
-
-
+	#if $RayCast2D.is_colliding()==false:
+		#direction=direction*-1
+		#$RayCast2D.position.x*=-1
+	#if get_slide_count()>0:
+		#for i in range(get_slide_count()):
+			#if "playercharactertest" in get_slide_collision(i).collider.name:
+				#get_slide_collision(i).collider.dead()
