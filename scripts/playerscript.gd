15808d8dafcefb8f0012ecb25ba3507041e547b7
matgib38 <82478253+matgib38@users.noreply.github.com>
2022-03-22 11:58:58 +1300

    fixed movement :D
    movement work

--- scripts/playerscript.gd
+++ scripts/playerscript.gd
@@ -1,9 +1,8 @@
 extends KinematicBody2D
 
-export var ACCELERATION = 500
-export var MAX_SPEED = 80
-export var ROLL_SPEED = 120
-export var FRICTION = 500
+const ACCELERATION = 850
+const MAX_SPEED = 200
+const FRICTION = 850
 
 enum {
 	MOVE,
@@ -12,18 +11,14 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
-var roll_vector = Vector2.DOWN
 #var stats = PlayerStats
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
-onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _physics_process(delta):
-	match state:
-		MOVE:
-			move_state(delta)
+	move_state(delta)
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -32,17 +27,16 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		roll_vector = input_vector
-		#swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
-		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
 
 func attack():
 	if Input.is_action_just_released("down"):
