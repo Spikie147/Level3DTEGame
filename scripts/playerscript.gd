dedcf74c0f7d93442973246c0130fad7e393e8b6
matgib38 <82478253+matgib38@users.noreply.github.com>
2022-03-28 09:57:58 +1300

    code change, also removed code

--- scripts/playerscript.gd
+++ scripts/playerscript.gd
@@ -58,46 +58,5 @@ func _on_sword_hit_area_entered(area):
 		area.damage()
 
 
-
-#func get_input():
-#	velocity = Vector2()
-#	#determines how the character moves based on player inputs
-#	#also plays specific animations depedning on the direction
-#	if Input.is_action_pressed("up"):
-#		velocity.y -= 1
-#		direction = Vector2(0, -1)
-#		$AnimationPlayer.play("walk_up")
-#		looking = "up"
-#	if Input.is_action_pressed("down"):
-#		velocity.y += 1
-#		direction = Vector2(0, 1)
-#		$AnimationPlayer.play("walk_down")
-#		looking = "down"
-#	if Input.is_action_pressed("left"):
-#		velocity.x -= 1
-#		direction = Vector2(-1, 0)
-#		$AnimationPlayer.play("walk_left")
-#		looking = "left"
-#	if Input.is_action_pressed("right"):
-#		velocity.x += 1
-#		direction = Vector2(1, 0)
-#		$AnimationPlayer.play("walk_right")
-#		looking = "right"
-#	#plays idle animation if the player is not moving
-#	else:
-#		if Input.is_action_just_released("up"):
-#			$AnimationPlayer.play("idle_up")
-#		if Input.is_action_just_released("down"):
-#			$AnimationPlayer.play("idle_down")
-#		if Input.is_action_just_released("left"):
-#			$AnimationPlayer.play("idle_left")
-#		if Input.is_action_just_released("right"):
-#			$AnimationPlayer.play("idle_right")
-#
-#	velocity = velocity.normalized()
-#	velocity = move_and_slide(velocity * 200)
-
-# This is all our old movement code that we moved from due to using an animation tree instead
-
 func _on_DetectPlayer_body_entered(body):
 	pass # Replace with function body.
