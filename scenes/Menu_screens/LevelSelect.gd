811a39b74a033eef634c510bbc57aaf14a28b83d
matgib38 <82478253+matgib38@users.noreply.github.com>
2022-03-30 12:25:20 +1300

    Fixed menu scene
    Also added new code for sound files

---
+++ scenes/Menu_screens/LevelSelect.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_Matthew_scene_pressed():
+	get_tree().change_scene("res://scenes/testroom_1.tscn")
+
+
+func _on_Cody_scene_pressed():
+	get_tree().change_scene("res://scenes/Enemycharctercodetest.tscn")
