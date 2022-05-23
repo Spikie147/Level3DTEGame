c242b734b20e2468c847f11f52bb94f35fd30695
matgib38 <82478253+matgib38@users.noreply.github.com>
2022-05-24 09:57:59 +1200

    escape key works

--- scripts/playerscript.gd
+++ scripts/playerscript.gd
@@ -24,6 +24,11 @@ onready var animationState = animationTree.get("parameters/playback")
 func _physics_process(delta):
 	attack()
 	move_state(delta)
+	menu()
+
+func menu():
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene("res://scenes/Menu_screens/MenuScreen.tscn")
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
