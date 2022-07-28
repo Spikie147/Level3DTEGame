f2594278f2906fb88f52bf521dfd28935495f2cf
matgib38 <82478253+matgib38@users.noreply.github.com>
2022-07-29 10:00:41 +1200

    updated pause menu a lot

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,8 +1,31 @@
 extends Control
 
+var timer
+var sound
+
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().change_scene("res://scenes/Menu_screens/MenuScreen.tscn")
+		if not visible:
+			visible = true
+		else:
+			visible = false
 
-func _ready():
-	pass 
+func _on_Resume_button_pressed():
+	if visible:
+		timer = get_node("Timer")
+		timer.set_wait_time(0.15)
+		timer.start()
+		sound = SoundPlayer.play_sound_effect("click")
+
+func _on_Menu_button_pressed():
+	timer = get_node("Menu_Timer")
+	timer.set_wait_time(0.15)
+	timer.start()
+
+func _on_Timer_timeout():
+	if 
+		if visible:
+			visible = false
+
+func _on_Menu_Timer_timeout():
+	get_tree().change_scene("res://scenes/Menu_screens/MenuScreen.tscn")
