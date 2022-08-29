a63b645fc843eb473df26fdcf13d56f8f200fe8f
matgib38 <82478253+matgib38@users.noreply.github.com>
2022-08-29 13:27:19 +1200

    attempted to fix coin pickup

--- entities/coin.gd
+++ entities/coin.gd
@@ -2,11 +2,10 @@ extends Node2D
 
 var player
 
-
 func _on_Area2D_area_entered(body):
 	if body.is_in_group("player"):
 		player = body
-		print("cody is trash")
+		print("code test")
 
 func _ready():
 	$AnimationPlayer.play("coin_spin")
